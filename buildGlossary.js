414897c35cc7af4356928bb65e0ccc0266e747fc
solareenlo <solareenlo@solareenlo.com>
2019-06-13 11:54:19 +0900

    update

--- buildGlossary.js
+++ buildGlossary.js
@@ -4,7 +4,7 @@ var termbase = fs.readFileSync('termbase.json');
 
 var termsObj = JSON.parse(termbase);
 
-function capitalize(string) 
+function capitalize(string)
 {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -17,7 +17,7 @@ function convertToDefinitionList (termsObj) {
     // Access categories, terms, and definitions
     for(var i = 0; i < termsObj.length; i++){
 
-        newFile.write('\r\n## ' + capitalize(termsObj[i].cat) + '\r\n\r\n' )
+        newFile.write('\r\n## ' + capitalize(termsObj[i].cat) + '\r\n\r\n' );
 
         for(term in termsObj[i].terms ){
             newFile.write('<dl><dt>' + term + '</dt>' + '<dd>' + termsObj[i].terms[term] + '</dd>' + '</dl>' + '\r\n');
